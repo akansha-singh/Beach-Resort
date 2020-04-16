5c41f0fcd58bb9681c9b755e43c0c9436a6523bf
akansha-singh <singhakansha281@gmail.com>
2020-04-17 02:49:06 +0530

    Loader added in Featured rooms

---
+++ src/components/FeaturedRooms.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {RoomContext} from "../context";
+import Loading from "./Loading";
+
+export default class FeaturedRooms extends Component {
+    static contextType = RoomContext;
+    render() {
+        const {FeaturedRooms: rooms} = this.context;
+        console.log(rooms);
+
+        return (
+            <div>
+                from featured rooms
+                <Loading />
+            </div>
+        );
+    }
+}
