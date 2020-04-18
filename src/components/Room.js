9140b98a977f4886c85cf6733c9c63d1854bf46b
akansha-singh <singhakansha281@gmail.com>
2020-04-18 13:34:24 +0530

    rooms images added

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import {Link} from "react-router-dom";
 
-export default function Room() {
+export default function Room({room}) {
+    const {name,slug,images,prices} = room;
     return (
-        <div>
-            hello from room
-        </div>
+        <article className="room">
+            <div className="img-container">
+                <img src={images[0]} alt="single room" />
+            </div>
+        </article>
     );
 }
